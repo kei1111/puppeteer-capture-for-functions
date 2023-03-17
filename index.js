ef1c78cf98372756ba585f53f76391e39c7a8a06
kei1111 <ke1taki6a25@outlook.jp>
2023-03-18 02:46:48 +0900

    update time out

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ exports.helloWorld = functions
     // });
     const iPhone = puppeteer.devices["iPhone X"];
     await page.emulate(iPhone);
+    await page.setDefaultNavigationTimeout(30000);
     await page.goto(url);
+    await page.waitForNavigation({ waitUntil: "load" });
     await page.addStyleTag({
       content: `body {font-family: "IPA Pゴシック","IPA PGothic" !important;}`,
     });
