a00456c583cf87d7e84f59ed8854ee76024d3075
kei1111 <ke1taki6a25@outlook.jp>
2023-04-25 23:31:45 +0900

    rm functions.logger.info

--- index.js
+++ index.js
@@ -10,28 +10,15 @@ exports.helloWorld = functions
   .region("asia-northeast1")
   .runWith(runtimeOpts)
   .https.onRequest(async (request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
     const url = request.query.url;
-
     const browser = await puppeteer.launch({
       headless: true,
-      args: [
-        // "--disable-gpu",
-        // "--disable-dev-shm-usage",
-        // "--disable-setuid-sandbox",
-        // "--no-first-run",
-        "--no-sandbox",
-        // "--no-zygote",
-        // "--single-process",
-        "--lang=ja,en-US,en",
-      ],
+      args: ["--no-sandbox", "--lang=ja,en-US,en"],
     });
     const page = await browser.newPage();
-    // await page.setExtraHTTPHeaders({
-    //   "Accept-Language": "ja-JP",
-    // });
     const iPhone = puppeteer.devices["iPhone X"];
-    await page.setDefaultNavigationTimeout(60000); //60秒に変更
+
+    await page.setDefaultNavigationTimeout(60000);
     await page.emulate(iPhone);
     await Promise.all([
       page.waitForNavigation({ waitUntil: "networkidle0", timeout: 60000 }),
